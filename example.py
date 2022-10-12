ff3ff49bccfee4d48bc947aa78a43d9c649000b4
Name <user@example.com>
2022-10-12 06:00:36 +0000

    CREATE NEW FILE

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-CREATE NEW FILE
+def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()CREATE NEW FILE
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
 git_opeation()
