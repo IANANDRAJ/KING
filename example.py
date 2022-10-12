9acbb9af4514cbd85ae59d4dd2a35783a8304e58
IANANDRAJ <115540910+IANANDRAJ@users.noreply.github.com>
2022-10-12 11:38:29 +0530

    Delete example.py

--- example.py
+++
@@ -1,6 +0,0 @@
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()CREATE NEW FILE
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()
